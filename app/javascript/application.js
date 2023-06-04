69307fc7987dc63cabc6f8bc60465021b94a70cc
saya <uknow.love1229@gmail.com>
2023-06-05 01:59:23 +0900

    chore: bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"; 
