4beff5ea9634136bae96c4998c1b5ddddff31dcd
saya <uknow.love1229@gmail.com>
2023-06-05 00:02:38 +0900

    chore: devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :end_users
+  devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
+
+  devise_for :end_users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
