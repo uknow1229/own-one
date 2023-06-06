3e4c27d1f058e774ba8b1d95704abad93eddce0f
saya <uknow.love1229@gmail.com>
2023-06-06 11:46:58 +0900

    feat: footer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,12 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 // import "channels";
+import "@rails/actioncable";
 
 import "jquery";
 import "popper.js";
-import "bootstrap";
-import "../stylesheets/application";
+// import "bootstrap";
+import "../stylesheets/application.scss";
 
 Rails.start()
 Turbolinks.start()
