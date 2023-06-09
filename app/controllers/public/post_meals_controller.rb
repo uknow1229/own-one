1991c76256fdfa97f2794e2e56921511c9c70819
saya <uknow.love1229@gmail.com>
2023-06-09 14:00:29 +0900

    feat: postmeals new

--- app/controllers/public/post_meals_controller.rb
+++ app/controllers/public/post_meals_controller.rb
@@ -3,9 +3,29 @@ class Public::PostMealsController < ApplicationController
     @post_meals = PostMeal.all
   end
 
+  def new
+    @post_meal = PostMeal.new
+  end
+
+  def create
+
+  end
+
   def show
   end
 
   def edit
   end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+
 end
--- app/models/post_meal.rb
+++ app/models/post_meal.rb
@@ -4,4 +4,8 @@ class PostMeal < ApplicationRecord
   has_many :meal_likes, dependent: :destroy
   has_many :mael_comments, dependent: :destroy
   has_many :mael_menus, dependent: :destroy
+
+  enum timing: { breakfast: 0, lunch: 1, dinner: 2, snacking: 3 }
+  enum meal_type: { cooking_for_oneself: 0, eating_out: 1, takeout: 2, convenience_store: 3 }
+
 end
