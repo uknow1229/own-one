639c7ad7555de67b5ba1483c637f9da4eb602355
saya <uknow.love1229@gmail.com>
2023-06-10 19:26:35 +0900

    fix: workout_likes asynchronization

--- app/controllers/public/workout_likes_controller.rb
+++ app/controllers/public/workout_likes_controller.rb
@@ -1,15 +1,15 @@
 class Public::WorkoutLikesController < ApplicationController
   def create
-    post_workout = PostWorkout.find(params[:post_workout_id])
-    like = current_end_user.workout_likes.new(post_workout_id: post_workout.id)
+    @post_workout = PostWorkout.find(params[:post_workout_id])
+    like = current_end_user.workout_likes.new(post_workout_id: @post_workout.id)
     like.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
-    post_workout = PostWorkout.find(params[:post_workout_id])
-    like = current_end_user.workout_likes.find_by(post_workout_id: post_workout.id)
+    @post_workout = PostWorkout.find(params[:post_workout_id])
+    like = current_end_user.workout_likes.find_by(post_workout_id: @post_workout.id)
     like.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 end
