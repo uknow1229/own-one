64936a2212ba968466cd6c822b88497973d38266
saya <uknow.love1229@gmail.com>
2023-06-10 16:51:07 +0900

    feat: meal_comments asynchronization

--- app/controllers/public/meal_comments_controller.rb
+++ app/controllers/public/meal_comments_controller.rb
@@ -1,15 +1,17 @@
 class Public::MealCommentsController < ApplicationController
   def create
-    post_meal = PostMeal.find(params[:post_meal_id])
-    comment = current_end_user.meal_comments.new(meal_comment_params)
-    comment.post_meal_id = post_meal.id
-    comment.save
-    redirect_to post_meal_path(post_meal)
+    @post_meal = PostMeal.find(params[:post_meal_id])
+    @comment = current_end_user.meal_comments.new(meal_comment_params)
+    @comment.post_meal_id = @post_meal.id
+    @comment.save
+    # redirect_to post_meal_path(post_meal)
   end
 
   def destroy
-    MealComment.find(params[:id]).destroy
-    redirect_to post_meal_path(params[:post_meal_id])
+    @comment = MealComment.find(params[:id])
+    @comment.destroy
+    @post_meal = PostMeal.find(params[:post_meal_id])
+    # redirect_to post_meal_path(params[:post_meal_id])
   end
 
   private
