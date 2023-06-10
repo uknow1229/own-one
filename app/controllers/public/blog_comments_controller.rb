ac706fe73bee1cdff9712cc64c615619fc170500
saya <uknow.love1229@gmail.com>
2023-06-10 18:11:20 +0900

    feat: blog_comments asynchronization

--- app/controllers/public/blog_comments_controller.rb
+++ app/controllers/public/blog_comments_controller.rb
@@ -4,12 +4,12 @@ class Public::BlogCommentsController < ApplicationController
     comment = current_end_user.blog_comments.new(blog_comment_params)
     comment.post_blog_id = post_blog.id
     comment.save
-    redirect_to post_blog_path(post_blog)
+    # redirect_to post_blog_path(post_blog)
   end
 
   def destroy
     BlogComment.find(params[:id]).destroy
-    redirect_to post_blog_path(params[:post_blog_id])
+    # redirect_to post_blog_path(params[:post_blog_id])
   end
 
   private
