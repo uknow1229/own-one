ca41f4550e810fcb9c521798c7ba2ba9eb74f687
saya <uknow.love1229@gmail.com>
2023-06-10 16:05:08 +0900

    perf: meal_likes asynchronization

--- app/controllers/public/meal_likes_controller.rb
+++ app/controllers/public/meal_likes_controller.rb
@@ -3,13 +3,13 @@ class Public::MealLikesController < ApplicationController
     post_meal = PostMeal.find(params[:post_meal_id])
     like = current_end_user.meal_likes.new(post_meal_id: post_meal.id)
     like.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
     post_meal = PostMeal.find(params[:post_meal_id])
     like = current_end_user.meal_likes.find_by(post_meal_id: post_meal.id)
     like.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 end
