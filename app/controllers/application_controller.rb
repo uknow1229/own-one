94df2028d6216f02216b89dc6babd6dfd36ed45d
saya <uknow.love1229@gmail.com>
2023-06-11 14:00:32 +0900

    feat: workout_search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :search
+
+  def search
+    @q = PostWorkout.ransack(params[:q])
+    @post_workout = @q.result
+    @result = params[:q]&.values&.reject(&:blank?)
+  end
+
 end
