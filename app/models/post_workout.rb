2310e609d0e4842d0766645e382085330a2b73f4
saya <uknow.love1229@gmail.com>
2023-06-12 18:02:51 +0900

    fix: workout_tags model

--- app/models/post_workout.rb
+++ app/models/post_workout.rb
@@ -37,4 +37,21 @@ class PostWorkout < ApplicationRecord
     [ "title"]
   end
 
+  def save_workout_tags(tags)
+    current_tags = self.workout_tags.pluck(:name) unless self.workout_tags.nil?
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+
+    # Destroy old taggings:
+    old_tags.each do |old_name|
+      self.workout_tags.delete WorkoutTag.find_by(name:old_name)
+    end
+
+    # Create new taggings:
+    new_tags.each do |new_name|
+      workout_tag = WorkoutTag.find_or_create_by(name:new_name)
+      self.workout_tags << workout_tag
+    end
+  end
+
 end
