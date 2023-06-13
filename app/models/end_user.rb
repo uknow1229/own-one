7b0aaeca2556b9ec53ec6ce20b601d3b43a4f832
saya <uknow.love1229@gmail.com>
2023-06-13 12:08:24 +0900

    feat: follow model

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -11,12 +11,18 @@ class EndUser < ApplicationRecord
   has_many :workout_comments, dependent: :destroy
 
   has_many :meal_likes, dependent: :destroy
-  # has_many :like_post_meals, through: :blog_likes, source: :post_meal
+  has_many :like_post_meals, through: :blog_likes, source: :post_meal
   has_many :meal_comments, dependent: :destroy
 
   has_many :blog_likes, dependent: :destroy
-  # has_many :like_post_blogs, through: :blog_likes, source: :post_blog
+  has_many :like_post_blogs, through: :blog_likes, source: :post_blog
   has_many :blog_comments, dependent: :destroy
+
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :following_user, through: follower, source: followed
+  has_many :follower_user, through: followed, source: follower
 
   enum sex: { woman: 0, man: 1, neither: 2, no_answer: 3 }
   enum activelevel: { level1: 0, level2: 1, level3: 2 }
@@ -41,6 +47,18 @@ class EndUser < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def follow(end_user_id)
+    follower.create(followed_id: end_user_id)
+  end
+
+  def unfollow(end_user.id)
+    follower.find_by(followed_id: end_user_id).destroy
+  end
+
+  def following?(end_user)
+    following_end_user.include?(end_user)
   end
 
 end
