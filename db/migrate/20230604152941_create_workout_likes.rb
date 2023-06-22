8249dc26bd93a3a062b53bc0db7131fcb57d16e2
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-06-22 12:50:25 +0000

    revert separate fk constraint[B

--- db/migrate/20230604152941_create_workout_likes.rb
+++ db/migrate/20230604152941_create_workout_likes.rb
@@ -1,12 +1,10 @@
 class CreateWorkoutLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :workout_likes do |t|
-      t.bigint :end_user_id, null: false
+      t.references :end_user, type: :bigint, foreign_key: true, null: false
       t.references :post_workout, type: :integer, foreign_key: true, null: false
 
       t.timestamps
-
-      add_foreign_key :workout_likes, :end_users
     end
   end
 end
