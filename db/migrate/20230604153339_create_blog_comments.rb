fcf50b62e256808befc9a659a6f240d14eaed412
uknow1229 <uknow.love1229@gmail.com>
2023-06-22 13:36:30 +0000

    use bigint

--- db/migrate/20230604153339_create_blog_comments.rb
+++ db/migrate/20230604153339_create_blog_comments.rb
@@ -1,12 +1,11 @@
 class CreateBlogComments < ActiveRecord::Migration[6.1]
   def change
     create_table :blog_comments do |t|
-      t.bigint :end_user_id, null: false
+      t.references :end_user, type: :bigint, null: false, foreign_key: true
       t.references :post_blog, type: :integer, null: false, foreign_key: true
       t.text :comment,    null: false
 
       t.timestamps
-      add_foreign_key :blog_comments :end_users
     end
   end
 end
