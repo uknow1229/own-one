ea393645526e96add97a1ab06d0a29771afb3e5c
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-06-22 12:46:52 +0000

    fix column mame

--- db/migrate/20230604152941_create_workout_likes.rb
+++ db/migrate/20230604152941_create_workout_likes.rb
@@ -1,7 +1,7 @@
 class CreateWorkoutLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :workout_likes do |t|
-      t.bigint :end_user, null: false
+      t.bigint :end_user_id, null: false
       t.references :post_workout, type: :integer, foreign_key: true, null: false
 
       t.timestamps
