08c6811e63cb38d2c2d3bd083a615f84f198c3d6
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-06-22 12:54:05 +0000

    unspecify type

--- db/migrate/20230604152941_create_workout_likes.rb
+++ db/migrate/20230604152941_create_workout_likes.rb
@@ -1,7 +1,7 @@
 class CreateWorkoutLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :workout_likes do |t|
-      t.references :end_user, type: :bigint, foreign_key: true, null: false
+      t.references :end_user, foreign_key: true, null: false
       t.references :post_workout, type: :integer, foreign_key: true, null: false
 
       t.timestamps
