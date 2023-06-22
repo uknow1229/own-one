0e83a406772d43ebe633e8940638d03d061361cb
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-06-22 12:09:50 +0000

    fix: migration

--- db/migrate/20230604153339_create_blog_comments.rb
+++ db/migrate/20230604153339_create_blog_comments.rb
@@ -1,11 +1,12 @@
 class CreateBlogComments < ActiveRecord::Migration[6.1]
   def change
     create_table :blog_comments do |t|
-      t.references :end_user, type: :bigint, null: false, foreign_key: true
+      t.bigint :end_user_id, null: false
       t.references :post_blog, type: :integer, null: false, foreign_key: true
       t.text :comment,    null: false
 
       t.timestamps
+      add_foreign_key :blog_comments :end_users
     end
   end
 end
