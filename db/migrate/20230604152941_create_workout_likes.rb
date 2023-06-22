63099342330a3c81fa329bb32d5072d3f7fecb89
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-06-22 13:57:09 +0000

    fix workout_likes.post_workout_id

--- db/migrate/20230604152941_create_workout_likes.rb
+++ db/migrate/20230604152941_create_workout_likes.rb
@@ -2,7 +2,7 @@ class CreateWorkoutLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :workout_likes do |t|
       t.references :end_user, type: :bigint, foreign_key: true, null: false
-      t.references :post_workout, type: :integer, foreign_key: true, null: false
+      t.references :post_workout, type: :bigint, foreign_key: true, null: false
 
       t.timestamps
     end
