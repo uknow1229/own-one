db29c4ff99b0dfaa15325541f5ed52ec9058d6be
saya <uknow.love1229@gmail.com>
2023-06-25 01:47:12 +0900

    fix: error messages partial

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -23,8 +23,8 @@ class Public::EndUsersController < ApplicationController
   def update
     @end_user = EndUser.find(params[:id])
     if @end_user.update(end_user_params)
-    redirect_to end_user_path
-    flash[:notice] = "更新が完了しました"
+      redirect_to end_user_path
+      flash[:notice] = "更新が完了しました"
     else
       flash[:notice] = "ユーザー情報を更新できませんでした"
       render :edit
