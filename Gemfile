76d00065753b5779a3e48c4ca78958948bf46e12
saya <uknow.love1229@gmail.com>
2023-08-06 19:00:20 +0900

    google map api2

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ group :development do
   gem 'spring'
 end
 
+
 group :test do
   # Adds support for Capybara system testing and selenium driver
   # gem 'capybara', '>= 3.26'
--- app/models/meal_comment.rb
+++ app/models/meal_comment.rb
@@ -3,14 +3,17 @@ class MealComment < ApplicationRecord
   belongs_to :post_meal
 
   has_one :notification, as: :subject, dependent: :destroy
+  # :as => :subjectの部分はこのモデルがポリモーフィック関連付けの一部であることを示している
 
   validates :comment, presence: true
 
   after_create_commit :create_notifications
+  # after_create_commitコールバック（コメントが作成され変更がDBにコミットされると呼び出される）
 
   private
-
+  # 新しい通知を作成
   def create_notifications
     Notification.create(subject: self, end_user: post_meal.end_user, action_type: :commented_to_meal_post)
+    # selfは現在のオブジェクトを指す(ポリモーフィック関連付けを使用しているので、subjectは異なるモデルを参照できる)
   end
 end
